335b5f55c7201a136044f1e44037d0a1500179ca
Tyler Mercer <acct@tylermercer.net>
2022-01-26 00:44:43 -0700

    Add basic GraphQL query for getting invite

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,21 +4,17 @@ import get from 'lodash/get'
 
 import Layout from '../components/layout'
 import Hero from '../components/hero'
-import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
+    const invite = get(this, 'props.data.contentfulWeddingInvite')
 
     return (
       <Layout location={this.props.location}>
         <Hero
-          image={author.heroImage.gatsbyImageData}
-          title={author.name}
-          content={author.shortBio.shortBio}
+          title={invite.introText}
+          content={invite.addendum.childMarkdownRemark.html}
         />
-        <ArticlePreview posts={posts} />
       </Layout>
     )
   }
@@ -28,44 +24,24 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      nodes {
-        title
-        slug
-        publishDate(formatString: "MMMM Do, YYYY")
-        tags
-        heroImage {
-          gatsbyImageData(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
-        description {
-          childMarkdownRemark {
-            html
-          }
+    contentfulWeddingInvite {
+      addendum {
+        childMarkdownRemark {
+          html
         }
       }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      nodes {
-        name
-        shortBio {
-          shortBio
-        }
-        title
-        heroImage: image {
-          gatsbyImageData(
-            layout: CONSTRAINED
-            placeholder: BLURRED
-            width: 1180
-          )
+      date(formatString: "MMMM Do, yyyy")
+      introText
+      nameJoiner
+      spouse1
+      spouse2
+      predicate {
+        childMarkdownRemark {
+          html
         }
       }
+      node_locale
     }
   }
+
 `
