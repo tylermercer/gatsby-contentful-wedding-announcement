44a94d347d3588cbb7d94e3d4244aada2d3efb04
Tyler Mercer <acct@tylermercer.net>
2022-02-23 21:36:58 -0700

    Update navigation
    I need to make the title text use the CMS....

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,18 +6,12 @@ import * as styles from './navigation.module.css'
 const Navigation = () => (
   <nav role="navigation" className={styles.container} aria-label="Main">
     <Link to="/" className={styles.logoLink}>
-      <span className={styles.logo} />
-      <span className={styles.navigationItem}>Gatsby Starter Contentful</span>
+      <span className={styles.navigationItem}>Populate me via Contentful!</span>
     </Link>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link to="/" activeClassName="active">
           Home
-        </Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/" activeClassName="active">
-          Blog
         </Link>
       </li>
     </ul>
