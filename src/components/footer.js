76e32eba24a31202a947ca2499e3f1826127964c
Tyler Mercer <acct@tylermercer.net>
2022-02-23 21:41:31 -0700

    Update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,8 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Built by <a href="https://github.com/tylermercer">Tyler Mercer</a> &middot;{' '}
+      <a href="https://github.com/tylermercer/gatsby-contentful-wedding-announcement">Source</a>
     </div>
   </Container>
 )
